023cd89d5a527a3b2bf4b37da068a9face7436db
Viraj <virajc@live.com>
2017-12-14 17:34:50 +0530

    the scroll thing
    scroll section by section

--- js/main.js
+++ js/main.js
@@ -83,3 +83,22 @@ $('#fixedbutton').click(function() {      // When arrow is clicked
 $("#register_icon").click(function(){
 	$('#register_modal').modal('toggle');
 })
+
+$.scrollify({
+    section : ".example-classname",
+    sectionName : "section-name",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+  });
